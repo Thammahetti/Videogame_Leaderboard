9f2dbd3f0293b076201fc7adffa446055a79d814
Thammahetti Thejan <thammahetti.m.t@iisgalvanimi.edu.it>
2024-03-18 23:04:58 +0000

    Finish 2.0

--- Videogame/src/app/app.component.ts
+++ Videogame/src/app/app.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ClassificaComponent } from './classifica/classifica.component';
-
+import {MatExpansionModule} from '@angular/material/expansion';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent,ClassificaComponent],
+  imports: [RouterOutlet, NavbarComponent,ClassificaComponent,MatExpansionModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Videogame';
+  xpandStatus=true;
+  
 }
